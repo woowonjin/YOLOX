26fcec541c72b1df10e3632a448ddafa7c79cc28
woowonjin <dndnjswls613@naver.com>
2022-02-20 18:57:40 +0900

    retrain_utils.py의 get_outputs_for_train 함수 완료

--- yolox/core/retrain_utils.py
+++ yolox/core/retrain_utils.py
@@ -17,14 +17,35 @@ class RetrainUtils:
         self.bcewithlog_loss = nn.BCEWithLogitsLoss(reduction="none")
         self.iou_loss = IOUloss(reduction="none")
         self.grids = [torch.zeros(1)] * len(self.in_channels)
+        self.num_classes = 11
+
+        self.use_l1 = False
 
     def split_output(self, output):
         return torch.split(output, [self.hw[0][0]*self.hw[0][1], self.hw[1][0]*self.hw[1][1], self.hw[2][0]*self.hw[2][1]], dim=1)
     
     def get_outputs_for_train(self, outputs):
+        origin_preds = []
+        x_shifts = []
+        y_shifts = []
+        expanded_strides = []
+        all_outputs = []
         for k, output in enumerate(outputs): # [[batch, 8160, 16], [batch, 2040, 16], [batch, 510, 16]]
             output, grid = self.get_output_and_grid(output, k, self.strides[k], self.xin_type)
-            print(grid)
+            x_shifts.append(grid[:, :, 0]) # grid : [... [x, y]]
+            y_shifts.append(grid[:, :, 1])
+            expanded_strides.append(
+                torch.zeros(1, grid.shape[1])
+                .fill_(self.strides[k])
+                .type(self.xin_type)
+            )
+            if self.use_l1:
+                batch_size = output.shape[0]
+                reg_output = output[:, :, :4].clone()
+                origin_preds.append(reg_output)
+            all_outputs.append(output)
+        return x_shifts, y_shifts, expanded_strides, torch.cat(all_outputs, 1), origin_preds, self.xin_type
+
 
     def get_output_and_grid(self, output, k, stride, dtype):
         grid = self.grids[k]
@@ -49,6 +70,7 @@ if __name__ == "__main__":
     util = RetrainUtils()
     preds = torch.randn(16, 10710, 16)
     splited = util.split_output(preds)
-    util.get_outputs_for_train(splited)
+    x_shifts, y_shifts, expanded_strides, outputs, origin_preds, dtype = util.get_outputs_for_train(splited)
+    # print(f"x_shifts : {x_shifts[0].size()}, y_shifts: {y_shifts[0].size()}, expanded_strides: {expanded_strides[0].size()}, outputs: {outputs.size()}, origin_preds: {origin_preds[0].size()}, dtype: {dtype}")
     
         
