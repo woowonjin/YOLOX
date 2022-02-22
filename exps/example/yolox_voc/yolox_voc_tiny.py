bdd3ab4b6eba77b43b10a7dd2b081e71e0fb39d7
woowonjin <dndnjswls613@naver.com>
2022-02-22 15:24:56 +0900

    change eval_interval 1 -> 5

--- exps/example/yolox_voc/yolox_voc_tiny.py
+++ exps/example/yolox_voc/yolox_voc_tiny.py
@@ -21,7 +21,7 @@ class Exp(MyExp):
         # self.test_size = (416, 416)
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         self.enable_mixup = False
-        self.eval_interval = 1
+        self.eval_interval = 5
         self.model = torch.load("/workspace/pruning/YOLOX/compressed_models/tiny_compressed.pt")
 
     def get_data_loader(self, batch_size, is_distributed, no_aug=False, cache_img=False):
